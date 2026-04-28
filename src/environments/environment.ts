85678bab123558fb8fe7778801c146a45478ad80
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-04-27 18:45:46 -0600

    Add A Juice Point lead config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { A_JUICE_POINT_CONFIG } from '../app/core/config/clients/a-juice-point.config';
 
 export const environment = {
   production: true,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: A_JUICE_POINT_CONFIG,
 };
