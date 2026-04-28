0a01c5804bd5f208cf749b7a0214e5ed6122ef81
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-04-28 13:53:09 -0600

    Add pancho-maiz lead config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { PANCHO_MAIZ_CONFIG } from '../app/core/config/clients/pancho-maiz.config';
 
 export const environment = {
   production: false,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: PANCHO_MAIZ_CONFIG,
 };
