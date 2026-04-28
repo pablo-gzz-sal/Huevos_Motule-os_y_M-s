08db471fb476fd852d6b46f3418fe53f73c35928
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-04-27 18:47:17 -0600

    Add DLU lead config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { DLU_CONFIG } from '../app/core/config/clients/dlu.config';
 
 export const environment = {
   production: false,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: DLU_CONFIG,
 };
