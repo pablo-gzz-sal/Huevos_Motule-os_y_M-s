73c95ace607a5c9eea0737398645711cf4137ebc
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-04-27 18:50:23 -0600

    Add El Tio Ricardo lead config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { EL_TIO_RICARDO_CONFIG } from '../app/core/config/clients/el-tio-ricardo.config';
 
 export const environment = {
   production: false,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: EL_TIO_RICARDO_CONFIG,
 };
