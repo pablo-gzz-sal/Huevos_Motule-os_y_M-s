513d8633014a112f9b4a50f56d6b5c25b9b9f44e
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-11 15:23:52 -0600

    Add Homu Merida lead config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+﻿import { HOMU_MERIDA_CONFIG } from '../app/core/config/clients/homu-merida.config';
 
 export const environment = {
   production: false,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: HOMU_MERIDA_CONFIG,
 };
+
