15c420f16994e03dba44479737db51779cbddcbb
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-11 15:24:27 -0600

    Add Viva La Brunch lead config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+﻿import { VIVA_LA_BRUNCH_CONFIG } from '../app/core/config/clients/viva-la-brunch.config';
 
 export const environment = {
   production: false,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: VIVA_LA_BRUNCH_CONFIG,
 };
+
