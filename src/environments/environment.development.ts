0a1c588b8b1fd6b70ebc7e546dea0cf3f4b4719e
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-12 16:11:18 -0600

    Activate parque-monchis-queretaro config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { PARQUE_MONCHIS_QUERETARO_CONFIG } from '../app/core/config/clients/parque-monchis-queretaro.config';
 
 export const environment = {
   production: false,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: PARQUE_MONCHIS_QUERETARO_CONFIG,
 };
