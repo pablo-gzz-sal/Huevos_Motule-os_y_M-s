00af9e9e030152e06034bcb0367221a00901eef8
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-12 16:11:16 -0600

    Activate casa-balvanera-queretaro config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { CASA_BALVANERA_QUERETARO_CONFIG } from '../app/core/config/clients/casa-balvanera-queretaro.config';
 
 export const environment = {
   production: true,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: CASA_BALVANERA_QUERETARO_CONFIG,
 };
