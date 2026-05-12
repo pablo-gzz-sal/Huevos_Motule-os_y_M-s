a3403e6525da79e279df90158d6716193c99be1c
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-12 16:11:18 -0600

    Activate vailima-cafe-cultura-queretaro config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { VAILIMA_CAFE_CULTURA_QUERETARO_CONFIG } from '../app/core/config/clients/vailima-cafe-cultura-queretaro.config';
 
 export const environment = {
   production: true,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: VAILIMA_CAFE_CULTURA_QUERETARO_CONFIG,
 };
