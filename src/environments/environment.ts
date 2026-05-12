da4233847f91a97e0d77056f9f7b904a9bb91da1
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-12 16:10:23 -0600

    Activate cafe-breton-queretaro config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+﻿import { CAFE_BRETON_QUERETARO_CONFIG } from '../app/core/config/clients/cafe-breton-queretaro.config';
 
 export const environment = {
   production: true,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: CAFE_BRETON_QUERETARO_CONFIG,
 };
+
