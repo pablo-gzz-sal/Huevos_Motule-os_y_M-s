f49830ab1e23dcddef1df607ed0d097cb078b7b4
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-12 16:10:20 -0600

    Activate querena-eatery-bar-centro-sur config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+﻿import { QUERENA_EATERY_BAR_CENTRO_SUR_CONFIG } from '../app/core/config/clients/querena-eatery-bar-centro-sur.config';
 
 export const environment = {
   production: false,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: QUERENA_EATERY_BAR_CENTRO_SUR_CONFIG,
 };
+
