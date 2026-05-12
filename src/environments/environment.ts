227830cd9f1622bb8af2c442e7cf36b92e423b96
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-12 16:11:15 -0600

    Activate brunch-terraza-queretaro config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { BRUNCH_TERRAZA_QUERETARO_CONFIG } from '../app/core/config/clients/brunch-terraza-queretaro.config';
 
 export const environment = {
   production: true,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: BRUNCH_TERRAZA_QUERETARO_CONFIG,
 };
