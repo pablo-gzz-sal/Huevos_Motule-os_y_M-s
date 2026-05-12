890283631a23555aa31746f9025f43ced841e0a7
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-12 16:10:52 -0600

    Activate casa-antonieta-queretaro config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { CASA_ANTONIETA_QUERETARO_CONFIG } from '../app/core/config/clients/casa-antonieta-queretaro.config';
 
 export const environment = {
   production: true,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: CASA_ANTONIETA_QUERETARO_CONFIG,
 };
