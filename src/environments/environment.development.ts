a1ff52722ce4bed6e51c026730882e535be9394d
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-12 16:11:19 -0600

    Activate paulino-cafe-queretaro config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+import { PAULINO_CAFE_QUERETARO_CONFIG } from '../app/core/config/clients/paulino-cafe-queretaro.config';
 
 export const environment = {
   production: false,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: PAULINO_CAFE_QUERETARO_CONFIG,
 };
