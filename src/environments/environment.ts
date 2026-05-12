7c75d4ec786278611a50472ed64713790dd3217d
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2026-05-12 16:10:21 -0600

    Activate queretaro-lindo-brunch-talleres config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
-import { COFFEE_ST_FITZ_CONFIG } from '../app/core/config/clients/coffee-st-fitz.config';
+﻿import { QUERETARO_LINDO_BRUNCH_TALLERES_CONFIG } from '../app/core/config/clients/queretaro-lindo-brunch-talleres.config';
 
 export const environment = {
   production: true,
-  activeBusinessConfig: COFFEE_ST_FITZ_CONFIG,
+  activeBusinessConfig: QUERETARO_LINDO_BRUNCH_TALLERES_CONFIG,
 };
+
